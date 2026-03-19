4cbe515bf5190c6b6dbfca7be2b0010fe73cee56
Frangargar <franciscolosalejos@gmail.com>
2026-03-19 16:46:33 +0100

    reorganizacion de ficheros

--- daily_snapshot.py
+++
@@ -1,7 +0,0 @@
-from save_market import save_daily_market
-from save_points import save_daily_points
-from build_dataset import build_dataset
-
-save_daily_market()
-save_daily_points()
-build_dataset()
